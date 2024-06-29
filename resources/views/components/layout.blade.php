9fc0f931eef536471a328250cb7f75412347e02f
moses197 <leomoszeko@gmail.com>
2024-06-29 17:12:48 +0100

    fsdfgDS

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/js/app.js'])
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
 </head>
 <body class="h-full">
 
